2760fb770a389697636dd7f2170d9149b7b3cae5
itwillwork <nurullin_edgar@mail.ru>
2016-03-11 22:34:53 +0300

    основная структура

--- server.js
+++ server.js
@@ -1,40 +1,16 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    mysql = require('mysql'),
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '5905'
+    }),
+    modelUser = require('./models/user'),
+    modelPost = require('./models/post'),
+    modelForum = require('./models/forum'),
+    modelThread = require('./models/thread');
 
 app.listen(80, function() {
     console.log('Все ОК. Слушаем на порту 80.');
 });
-
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '5905'
-});
-
-
-app.route('/db/api/')
-    .get(function(req, res) {
-        res.status(200);
-
-        connection.connect();
-
-        connection.query('SHOW DATABASES', function(err, rows, fields) {
-            if (err)
-                throw err;
-
-            console.log('The solution is: ', rows);
-            res.json({
-                "code": 200,
-                "response": rows
-            });
-        });
-
-        connection.end();
-    })
-    .post(function(req, res) {
-        res.send('Add a book');
-    })
-    .put(function(req, res) {
-        res.send('Update the book');
-    });
