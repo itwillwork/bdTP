eb1427a6dbf4816bf05fe626559e8df4443cf852
itwillwork <nurullin_edgar@mail.ru>
2016-03-13 01:27:26 +0300

    updateProfile

--- models/user.js
+++ models/user.js
@@ -57,32 +57,10 @@ module.exports.details = function(dataObject, responceCallback) {
 					if (err) callback( helper.mysqlError(err.errno) , null);
 					else callback(null, res);
 				});
-		},
-		function (callback) {
-			//TODO folowers
-			callback(null, null);
-		},
-		function (callback) {
-			//TODO folowers
-			callback(null, null);
-		},
-		function (callback) {
-			//TODO subscriptions
-			callback(null, null);
 		}
 	], function(err, res) {
 		if (err) responceCallback(err.code, err.message);
-		else responceCallback(0, {
-			"about": res[1][0].about,
-			"email": res[1][0].email,
-			"followers": [],
-			"following": [],
-			"id": res[1][0].id,
-			"isAnonymous": !!(res[1][0].isAnonymous),
-			"name": res[1][0].name,
-			"subscriptions": [],
-			"username": res[1][0].username 
-		});	
+		else module.exports.moreDetails(res[1][0], {user: res[1][0].email}, {user: res[1][0].email}, responceCallback);
 	});
 }
 
@@ -208,6 +186,76 @@ module.exports.updateProfile = function(dataObject, responceCallback) {
 					if (err) callback( helper.mysqlError(err.errno) , null);
 					else module.exports.details({user: dataObject.user}, responceCallback);
 				});
+		}
+	});
+}
+
+/**
+ * составитель запросов для user.listFollowers и user.listFollowing
+ */
+function getSQLForFollowers (target, wherefore, wherefrom) {
+	var sql = 'SELECT ' + target + ' FROM followers ';
+	if (wherefrom.order !== 'asc') {
+		wherefrom.order = 'desc';
+	}
+	sql += ' WHERE ' + wherefore + ' = ? ';
+	if (wherefrom.since_id) {
+		sql += ' AND id >= ' + wherefrom.since_id
+	}
+	sql += ' ORDER BY ' + target + ' ' + wherefrom.order;
+	if (wherefrom.limit) {
+		sql += ' LIMIT ' + wherefrom.limit;
+	}
+	return sql;
+} 
+
+module.exports.moreDetails = function(dataObject, listFollowers, listFollowing, responceCallback) {
+	async.parallel([
+		function (callback) {		
+			connection.db.query( getSQLForFollowers('followeeEmail', 'followerEmail', listFollowers),
+				[listFollowers.user], 
+				function(err, res) {
+					if (err) {
+						callback( helper.mysqlError(err.errno) , null);
+					} else {
+						callback(null, res);
+					}
+				});
+		},
+		function (callback) {
+			connection.db.query( getSQLForFollowers('followerEmail', 'followeeEmail', listFollowers),
+				[listFollowers.user], 
+				function(err, res) {
+					if (err) {
+						callback( helper.mysqlError(err.errno) , null);
+					} else {
+						callback(null, res);
+					}
+				});
+		},
+		function (callback) {
+			//TODO subscribes
+			callback(null, null);
+		}
+	],
+	function(err, results){
+		if (err) responceCallback(err.code, err.message);
+		else {
+			responceCallback(0, {
+			"about": dataObject.about,
+			"email": dataObject.email,
+			"followers": results[1].map(function(elem) {
+						  return elem.followerEmail;
+						}),
+			"following": results[0].map(function(elem) {
+						  return elem.followeeEmail;
+						}),
+			"id": dataObject.id,
+			"isAnonymous": !!(dataObject.isAnonymous),
+			"name": dataObject.name,
+			"subscriptions": [],
+			"username": dataObject.username 
+		});	
 		}
 	});
 }
