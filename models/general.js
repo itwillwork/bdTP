eef0f46005b23d0e5edb994b2ac142b98900de19
itwillwork <nurullin_edgar@mail.ru>
2016-03-13 21:51:37 +0300

    general status, general clear

--- models/general.js
+++ models/general.js
@@ -4,7 +4,6 @@ var connection = require('./../connection'),
 
 module.exports.clear = function (responceCallback) {
 	async.parallel([
-		/*
 		function(callback){
 			connection.db.query("TRUNCATE TABLE user", 
 				function(err, res) {
@@ -18,10 +17,30 @@ module.exports.clear = function (responceCallback) {
 					if (err) callback( helper.mysqlError(err.errno) , null);
 					else callback(null, res);
 				});
-		}
-		*/
+		},
 		function(callback){
 			connection.db.query("TRUNCATE TABLE thread", 
+				function(err, res) {
+					if (err) callback( helper.mysqlError(err.errno) , null);
+					else callback(null, res);
+				});
+		},
+		function(callback){
+			connection.db.query("TRUNCATE TABLE subscribes", 
+				function(err, res) {
+					if (err) callback( helper.mysqlError(err.errno) , null);
+					else callback(null, res);
+				});
+		},
+		function(callback){
+			connection.db.query("TRUNCATE TABLE followers", 
+				function(err, res) {
+					if (err) callback( helper.mysqlError(err.errno) , null);
+					else callback(null, res);
+				});
+		},
+		function(callback){
+			connection.db.query("TRUNCATE TABLE post", 
 				function(err, res) {
 					if (err) callback( helper.mysqlError(err.errno) , null);
 					else callback(null, res);
@@ -35,5 +54,43 @@ module.exports.clear = function (responceCallback) {
 }
 
 module.exports.status = function (responceCallback) {
-	//TODO create method
+	async.parallel([
+		function(callback){
+			connection.db.query("SELECT COUNT(*) AS count FROM user", 
+				function(err, res) {
+					if (err) callback( helper.mysqlError(err.errno) , null);
+					else callback(null, res);
+				});
+		},
+		function(callback){
+			connection.db.query("SELECT COUNT(*) AS count FROM thread", 
+				function(err, res) {
+					if (err) callback( helper.mysqlError(err.errno) , null);
+					else callback(null, res);
+				});
+		},
+		function(callback){
+			connection.db.query("SELECT COUNT(*) AS count FROM forum", 
+				function(err, res) {
+					if (err) callback( helper.mysqlError(err.errno) , null);
+					else callback(null, res);
+				});
+		},
+		function(callback){
+			connection.db.query("SELECT COUNT(*) AS count FROM post", 
+				function(err, res) {
+					if (err) callback( helper.mysqlError(err.errno) , null);
+					else callback(null, res);
+				});
+		}
+	],
+	function(err, results){
+		if (err) responceCallback(err.code, err.message);
+		else responceCallback(0, {
+			'user': results[0][0].count,
+			'thread': results[1][0].count,
+			'forum': results[2][0].count,
+			'post': results[3][0].count
+		});	
+	});
 }
