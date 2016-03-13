3566993bd827117d1c2f1dafc2b3bf8ec97ff8ad
itwillwork <nurullin_edgar@mail.ru>
2016-03-13 20:09:56 +0300

    thread open

--- models/thread.js
+++ models/thread.js
@@ -68,8 +68,13 @@ module.exports.listPosts =  function(dataObject, responceCallback) {
 }
 
 module.exports.open =  function(dataObject, responceCallback) {
-	//TODO реализовать
-	responceCallback(0, "метод еще не реализован")
+	connection.db.query('UPDATE thread SET isClosed = false WHERE id = ?', 
+		[dataObject.thread], 
+		function(err, res) {
+			if (err) err = helper.mysqlError(err.errno);
+			if (err) responceCallback(err.code, err.message);
+			responceCallback(0, dataObject);
+		});
 }
 
 module.exports.remove =  function(dataObject, responceCallback) {
