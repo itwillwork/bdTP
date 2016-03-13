042e794823a01a3411c3c964a46be6bec0693793
itwillwork <nurullin_edgar@mail.ru>
2016-03-13 19:03:55 +0300

    routing thread

--- models/thread.js
+++ models/thread.js
@@ -1,9 +1,65 @@
 var connection = require('./../connection'),
-    response = {
-        "code": 0,
-        "response": {
-            "field": "value"
-        }
-    };
+	helper = require('./../helper'),
+	async = require('async'),
+	userModel = require('./user.js'),
+	error = helper.errors;
 
-module.exports.response = response;
+module.exports.close =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.create =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.details =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.list =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.listPosts =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.open =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.remove =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.restore =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.subscribe =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.unsubscribe =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.update =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
+
+module.exports.vote =  function(dataObject, responceCallback) {
+	//TODO реализовать
+	responceCallback(0, "метод еще не реализован")
+}
--- routers/thread.js
+++ routers/thread.js
@@ -1,9 +1,54 @@
 var express = require('express'),
     router = express.Router(),
-    model = require('./../models/thread');
+    model = require('./../models/thread'),
+    responceCallback = require('./../responce');
 
-router.get('/', function(req, res) {
-    res.send('thread routing');
+router.post('/close', function(req, res) {
+    model.close(req.body, responceCallback(res));
+});
+
+router.post('/create', function(req, res) {
+    model.create(req.body, responceCallback(res));
+});
+
+router.get('/details', function(req, res) {
+    model.details(req.query, responceCallback(res));
+});
+
+router.get('/list', function(req, res) {
+    model.list(req.query, responceCallback(res));
+});
+
+router.get('/listPosts', function(req, res) {
+    model.listPosts(req.query, responceCallback(res));
+});
+
+router.post('/open', function(req, res) {
+    model.open(req.body, responceCallback(res));
+});
+
+router.post('/remove', function(req, res) {
+    model.remove(req.body, responceCallback(res));
+});
+
+router.post('/restore', function(req, res) {
+    model.restore(req.body, responceCallback(res));
+});
+
+router.post('/subscribe', function(req, res) {
+    model.subscribe(req.body, responceCallback(res));
+});
+
+router.post('/unsubscribe', function(req, res) {
+    model.unsubscribe(req.body, responceCallback(res));
+});
+
+router.post('/update', function(req, res) {
+    model.update(req.body, responceCallback(res));
+});
+
+router.post('/vote', function(req, res) {
+    model.vote(req.body, responceCallback(res));
 });
 
 module.exports = router;
