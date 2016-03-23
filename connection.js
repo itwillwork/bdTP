b82f363bd218b29dffd2496ac438b842e04d174d
itwillwork <nurullin_edgar@mail.ru>
2016-03-23 03:05:49 +0300

    connection popooling

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql'),
-    connection = mysql.createConnection({
-        host: 'localhost',
+	pool  = mysql.createPool({
+		connectionLimit : 10,
+		host: 'localhost',
         user: 'root',
         password: '5905',
         database: 'dbfortphomework'
-    });
-connection.connect();
-module.exports.db = connection;
+	});
+module.exports.db = pool;
